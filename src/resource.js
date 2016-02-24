8cbbc8ca51fa655f8f4ac4264345d0f9a8f019b3
gurudiel <gurudiel@users.noreply.github.com>
2016-02-23 23:33:23 -0400

    Tarea 2 TicTacToe

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,29 @@
+/* 
+ * Adaptación del TicTacToe V1.0 en base al proyecto
+   de grayfire.
+ */
+ 
 var res = {
-    O_png : "res/O.png",
-    X_png : "res/X.png",
-    tablero_png : "res/tablero.png"
+    Btn_png : "res/btn.png",
+    Btn_s_png : "res/btn-s.png",
+    BackGround_png : "res/tablero.png",
+    o_png : "res/o.png",
+    x_png : "res/x.png",
+    Empty_png : "res/empty.png",
+    WinMark_png : "res/win-mark.png",
+    Play_png : "res/play.png",
+    Play_s_png : "res/play-s.png",
+    CloseBtn_png : "res/close-btn.png",
+    CloseBtn_s_png : "res/close-btn-s.png",
+    RadioBtn_png : "res/radio-btn.png",
+    RadioBtn_s_png : "res/radio-btn-s.png",
+    Round_r_png : "res/round-r.png",
+    Round_r_s_png : "res/round-r-s.png",
+    Round_l_png : "res/round-l.png",
+    Round_l_s_png : "res/round-l-s.png",
+    UpBtn_png : "res/up-btn.png",
+    UpBtn_s_png : "res/up-btn-s.png",
+    Selected_png : "res/selected.png"
 };
 
 var g_resources = [];
