8007a88c372aab7968c0579d90064026c355f128
gurudiel <gurudiel@users.noreply.github.com>
2016-02-23 23:30:47 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,45 +1,37 @@
 
-var HelloWorldLayer = cc.Layer.extend({
+/* 
+ * Adaptación del TicTacToe V1.0 en base al proyecto
+   de grayfire.
+ */
+ 
+var TopMenuLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
         var size = cc.winSize;
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("TicTacToe V0.1", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        var titleLabel = new cc.LabelTTF("TicTacToe V0.1", "Arial", 32);
+        titleLabel.x = size.width / 2;
+        titleLabel.y = size.height - 40;
+        this.addChild(titleLabel, 5);
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.tablero_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
-
+               
         return true;
-    }
+    },
+
+    onEnter : function(){
+        cc.director.runScene(new TttxScene(3));
+    },
+
+   
+
 });
 
-var HelloWorldScene = cc.Scene.extend({
+
+var TopMenuScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new TopMenuLayer();
         this.addChild(layer);
     }
 });
-
